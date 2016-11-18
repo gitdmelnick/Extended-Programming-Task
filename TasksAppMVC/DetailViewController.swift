57510f533ceb894f43b13d89cf7881d477762965
Dmitriy Melnichenko <dmitriymelnichenko@Dmitriys-MacBook-Pro.local>
2016-11-18 12:36:18 +0200

    Added text view scroll during keyboard input

--- TasksAppMVC/DetailViewController.swift
+++ TasksAppMVC/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextViewDelegate {
 
     var currentTask: Task!
     
@@ -22,13 +22,39 @@ class DetailViewController: UIViewController {
             textView.text = currentTask.taskDescription
         }
         
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(DetailViewController.dismissKeyboard))
+        self.view.addGestureRecognizer(gestureRecognizer)
+        
+        self.automaticallyAdjustsScrollViewInsets = false
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.backAction()
     }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let line = textView.caretRect(for: (textView.selectedTextRange?.start)!)
+        let overflow = line.origin.y + line.size.height - (textView.contentOffset.y + textView.bounds.size.height * 0.55 - textView.contentInset.bottom - textView.contentInset.top)
+        if overflow > 0 {
+            var offset = textView.contentOffset
+            offset.y += overflow + 7
+            textView.setContentOffset(offset, animated: false)
+        }
+    }
+    
 }
+
+extension DetailViewController {
+    func dismissKeyboard() {
+        self.textView.resignFirstResponder()
+    }
+}
+
 
 extension DetailViewController: Dismissable {
     
